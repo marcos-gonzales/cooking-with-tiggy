179eff0b299f6f2a8f5a262cfdae1e3e8678fcc2
Marcos Gonzales <markymarrk@gmail.com>
2022-03-30 17:01:43 -0600

    removed heroku config

--- config/database.php
+++ config/database.php
@@ -2,13 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$url = parse_url(getenv("mysql://b6eb06d752bf93:027fe8eb@us-cdbr-east-05.cleardb.net/heroku_957f89d4c6dac34?reconnect=true"));
-
-$host = $url["host"] ?? null;
-$username = $url["user"] ?? null;
-$password = $url["pass"] ?? null;
-$database = substr($url["path"], 1);
-
 return [
 
     /*
@@ -97,19 +90,6 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
-
-
-        'your_heroku_mysql_connection' => array(
-            'driver' => 'mysql',
-            'host' => 'us-cdbr-east-05.cleardb.net/heroku_957f89d4c6dac34?reconnect=true',
-            'database' => 'mysql',
-            'username' => 'mysql://b6eb06d752bf93',
-            'password' => '027fe8eb',
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-        ),
-
     ],
 
     /*
